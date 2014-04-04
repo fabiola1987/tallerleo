d893d8788f1c05c5a402f8ef96aa4387e48b6b13
fabiola1987 <joshinoj@alumnos.ubiobio.cl>
2014-04-04 10:44:57 -0400

    rama
    ajksgbdkl

--- hinojosa/src/hinojosa/Hinojosa.java
+++ hinojosa/src/hinojosa/Hinojosa.java
@@ -10,6 +10,7 @@ public class Hinojosa {
     public static void main(String[] args) {
         System.out.println("Segundo");
         System.out.println("Esto lo agregue por internet");
+        System.out.println("Rama");
         // TODO code application logic here
     }
 }
