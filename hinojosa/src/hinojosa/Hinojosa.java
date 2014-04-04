44d9a9fb87a265701e8eda11352720f8147b0149
fabiola1987 <joshinoj@alumnos.ubiobio.cl>
2014-04-04 11:18:25 -0400

    Update Hinojosa.java
    cambiio

--- hinojosa/src/hinojosa/Hinojosa.java
+++ hinojosa/src/hinojosa/Hinojosa.java
@@ -8,7 +8,7 @@ public class Hinojosa {
 
   
     public static void main(String[] args) {
-        System.out.println("Segundo");
+        System.out.println("Segundo desde web");
         System.out.println("Esto lo agregue por internet");
         // TODO code application logic here
     }
